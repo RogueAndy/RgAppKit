69ad6724c054ca1a9f124b510bf60a6b406d857e
Rogue Andy <zlj_1969_love@126.com>
2017-08-29 15:40:51 +0800

    导入POP

--- RApps/RApps/ViewControllers/RAFourthVC.swift
+++ RApps/RApps/ViewControllers/RAFourthVC.swift
@@ -7,11 +7,24 @@
 //
 
 import UIKit
+import pop
 
 class RAFourthVC: RABaseVC {
 
     override func loadViews() {
+        super.loadViews()
         view.backgroundColor = .gray
+        
+        let btn = UIButton(frame: CGRect(x: (UIScreen.main.bounds.width - 200) / 2.0, y: 100, width: 200, height: 40))
+        btn.backgroundColor = .orange
+        btn.addTarget(self, action: #selector(animation1), for: .touchUpInside)
+        view.addSubview(btn)
+    }
+    
+    @objc private func animation1() {
+    
+        let popObject = POP
+    
     }
 
 }
