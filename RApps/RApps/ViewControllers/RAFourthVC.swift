3266116f8b9f28266f17f67abf3c568ca8bba556
Rogue Andy <zlj_1969_love@126.com>
2017-08-29 16:23:20 +0800

    弹性动画

--- RApps/RApps/ViewControllers/RAFourthVC.swift
+++ RApps/RApps/ViewControllers/RAFourthVC.swift
@@ -11,19 +11,46 @@ import pop
 
 class RAFourthVC: RABaseVC {
 
+    private var fullSizeLayer: CALayer!
+    
     override func loadViews() {
         super.loadViews()
         view.backgroundColor = .gray
         
-        let btn = UIButton(frame: CGRect(x: (UIScreen.main.bounds.width - 200) / 2.0, y: 100, width: 200, height: 40))
-        btn.backgroundColor = .orange
+        let btn = UIButton(frame: CGRect(x: (UIScreen.main.bounds.width - 100) / 2.0, y: 100, width: 100, height: 35))
+        btn.setTitle("放大", for: .normal)
+        btn.setTitleColor(.black, for: .normal)
         btn.addTarget(self, action: #selector(animation1), for: .touchUpInside)
         view.addSubview(btn)
+        
+        let scale = UIButton(frame: CGRect(x: (UIScreen.main.bounds.width - 100) / 2.0, y: 150, width: 100, height: 35))
+        scale.setTitle("缩小", for: .normal)
+        scale.setTitleColor(.black, for: .normal)
+        scale.addTarget(self, action: #selector(animation2), for: .touchUpInside)
+        view.addSubview(scale)
+        
+        fullSizeLayer = CALayer()
+        fullSizeLayer.frame = CGRect(x: (UIScreen.main.bounds.width - 200) / 2.0, y: 200, width: 40, height: 40)
+        fullSizeLayer.backgroundColor = UIColor.green.cgColor
+        view.layer.addSublayer(fullSizeLayer)
+        
     }
     
     @objc private func animation1() {
+        
+        let spring_animation = POPSpringAnimation(propertyNamed: kPOPLayerSize)
+        spring_animation?.springBounciness = 19
+        spring_animation?.toValue = NSValue(cgSize: CGSize(width: 80, height: 80))
+        fullSizeLayer.pop_add(spring_animation, forKey: "size")
     
-        let popObject = POP
+    }
+    
+    @objc private func animation2() {
+    
+        let spring_animation = POPSpringAnimation(propertyNamed: kPOPLayerSize)
+        spring_animation?.springBounciness = 20
+        spring_animation?.toValue = NSValue(cgSize: CGSize(width: 40, height: 40))
+        fullSizeLayer.pop_add(spring_animation, forKey: "size")
     
     }
 
